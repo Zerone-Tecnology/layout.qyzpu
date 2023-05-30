49072bdb5229b8d7bd7d769cb7b028504b3e04f6
Damir Rashidinov <damir.dmr88@gmail.com>
2023-05-30 17:41:19 +0600

    add anima

--- js/common.js
+++ js/common.js
@@ -1,5 +1,19 @@
 $(function() {
 
+	document.addEventListener("mousemove", parallax);
+	function parallax(e){
+		this.querySelectorAll('.img-g').forEach(layer => {
+			const speed = layer.getAttribute('data-speed')
+
+			const x = (window.innerWidth - e.pageX*speed)/100
+			const y = (window.innerHeight - e.pageY*speed)/100
+
+			layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+		})
+	}
+
+	new WOW().init();
+
 	$('.slider').owlCarousel({
 		items: 1
 	});
